16e7197ad6e56e6e3ba9a11e47ed032cb6da176c
Еремей Миронов <yerem01@mail.ru>
2022-05-29 22:03:51 +0300

    123

--- Laba_opt_6/Form1.cs
+++ Laba_opt_6/Form1.cs
@@ -16,10 +16,10 @@ namespace Laba_opt_6
         {
             double M = 0;
             double x = 1;
-            for (int i = 0; i<200;i++)
+            for (int i = 0; i < 200; i++)
             {
-                x = Math.Pow(5, 10) * x % Math.Pow(2, 16);
-                M = M + ((x / Math.Pow(2, 16)) - 0.5);
+
+                M = M + (X[i]);
             }
             return M / N;
         }
@@ -28,50 +28,78 @@ namespace Laba_opt_6
             double Q = 0;
             for (int i = 0; i < N; i++)
             {
-                Q = Q + Math.Pow(xi(N) - Ni(200), 2);
+                Q = Q + Math.Pow(X[i] - Ni(N), 2);
+            }
+            return Q / N;
+        }
+        public double Niz(int N)
+        {
+            double M = 0;
+            double x = 1;
+            for (int i = 0; i < N; i++)
+            {
+
+                M = M + (Z[i]);
+            }
+            return M / N;
+        }
+        public double Qiz(int N)
+        {
+            double Q = 0;
+            for (int i = 0; i < N; i++)
+            {
+                Q = Q + Math.Pow(Z[i] - Niz(190), 2);
             }
             return Q / N;
         }
         public double xi(int I)
         {
+            
             double x = 1;
             for (int i = 0; i < I; i++)
             {
                 x = Math.Pow(5, 10) * x % Math.Pow(2, 16);
-                
+                X.Add((x / Math.Pow(2, 16)) - 0.5);
             }
+            
            return (x / Math.Pow(2, 16)) - 0.5;
         }
         public double zi(int k)
         {
-            double Z = 0;
+            double z = 0;
 
             for (int j = 0; j < k-10 ;j++)
             {
-                Z = 0;
+                z = 0;
                 for (int i = j; i < j + 10; i++)
                 {
-                    Z = Z + (xi(i) * (Math.Pow(2500.0 / (Qi(k) * 0.12), 0.5) * Math.Exp( -0.12 * (j - i))+200));
+                    z = z + (xi(i) * (Math.Sqrt(2500.0 / (Qi(k) * 0.12*19125)) * Math.Exp( -0.12 * 0.025 * (j - i))));
                     
                 }
-                chart2.Series[0].Points.AddXY(j, (Z / 10));
+                Z.Add(z / 10);
             }
             
-            return( Z / 10);
+            return( z / 10);
         }
         //Math.Log(k / (Qi(200)*(20 - s)))
         public double Ki(int s)
         {
             double k = 0;
-            for (int i = 1; i < 200 - s; i++)
+            for (int i = 1; i < 190 - s; i++)
             {
-                k = k + (xi(i) - Ni(i)) * (xi(s + i) - Ni(i));
-
-                chart4.Series[0].Points.AddXY(i, k / (200 - s));
+                k = k + ((X[i] - Ni(200)) * (X[ s+i ] - Ni(200)));
+                K.Add( k / (190 - s));
+                A.Add(Math.Abs(Math.Log(-K[i-1]/Qi(200))/s));
 
             }
-            return k / (200-s);
+            return k / (190-s);
         }
+       
+        public List<double> X = new List<double>();
+        public List<double> Z = new List<double>();
+        public List<double> K = new List<double>();
+        public List<double> A = new List<double>();
+
         public Form1()
         {
             InitializeComponent();
@@ -84,28 +112,36 @@ namespace Laba_opt_6
             double z0 = 1;
             double k = 20;
             double x = 1;
+            xi(200);
             for (int i = 0; i < 200; i++)
             {
-                x = Math.Pow(5, 10) * x % Math.Pow(2, 16);
-
-                chart1.Series[0].Points.AddXY(i, (x / Math.Pow(2, 16)) - 0.5);
-
-                
+                chart1.Series[0].Points.AddXY(i, X[i]);
             }
-            for (int i = 0; i < 20; i++)
+            Console.WriteLine("M(x) = " + Ni(200));
+            Console.WriteLine("Q(x) = " + Qi(200));
+
+            zi(200);
+            for (int i = 0; i < 190; i++)
             {
-                k = Qi(200) * Math.Exp(-0.12 * Math.Abs(i));
-                chart3.Series[0].Points.AddXY(i,k);
-
-
+                chart2.Series[0].Points.AddXY(i, Z[i]);
             }
+            Console.WriteLine("Mz(x) = " + Niz(190));
+            Console.WriteLine("Qz(x) = " + Qiz(190));
+
+            int n = 0;
+
+            Ki(20);
+            foreach (double d in K)
+            {
+                chart3.Series[0].Points.AddXY(n, K[n]);
+                chart4.Series[0].Points.AddXY(n, A[n]);
+                n = n + 1;
+            }
+            Console.WriteLine("a = " + A[n-1]);
 
 
-            Console.WriteLine(Ni(200));
-            Console.WriteLine(Qi(200));
-            Console.WriteLine(zi(200));
-            Console.WriteLine(Ki(20));
-            
+
+
         }
     }
 }
